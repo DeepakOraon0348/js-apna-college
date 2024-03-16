3f04cd5bc6087722f6fcebba69e512f4e95b4093
Deepak Oraon <162132343+DeepakOraon0348@users.noreply.github.com>
2024-03-16 01:06:17 -0700

    Add files via upload

---
+++ array.js
@@ -0,0 +1,81 @@
+// let marks=[97,82,75,64,36];
+// console.log(marks);
+// console.log(marks.length);
+
+
+//let heroes=["ironman","thor","hulk","shaktiman","spiderman","antman"];
+// for(let index=0;index<heroes.length;index++){
+//     console.log(heroes[index]);
+// }
+
+
+//for of
+
+// for(let element of heroes){
+//     console.log(element.toUpperCase());
+// }
+
+// let marks=[85,97,44,37,76,60];
+// let sum=0;
+
+// for(let val of marks){
+//     sum+=val;
+// }
+
+// let avg=sum/marks.length;
+// console.log(`avg marks of the class = ${avg}`);
+
+//let items=[250,645,300,900,50];
+
+// let i=0;
+
+// for(let val of items){
+//     console.log(`value at index ${val}`);
+//     let offer=val/10;
+//     items[i]=items[i]-offer;
+//     console.log(`value ofter offer = ${items[i]}`);
+//     i++;
+// }
+
+
+//using for loop
+// for(let i=0;i<items.length;i++){
+//     let offer=items[i]/10;
+//     items[i]-=offer;
+// }
+
+// console.log(items);
+
+//**************pop method */
+
+// let fooditems=["potato","apple","litchi","tomato"];
+// console.log(fooditems);
+// let deletedItem=fooditems.pop();
+// console.log(fooditems);
+// console.log(deletedItem);
+/************************* toString method */
+
+// let fooditems=["potato","apple","litchi","tomato"];
+// console.log(fooditems); 
+// console.log(fooditems.toString());
+// console.log(fooditems);
+
+
+// let marvelheroes=["thor","spiderman","ironman"];
+// let dcHeroes=["superman","batman"];
+
+// let heroes=marvelheroes.concat(dcHeroes);
+// console.log(heroes);
+
+//marvelheroes.unshift("antman");
+
+//********************finding current date and time************** */
+
+// let date;
+// date=new Date().toLocaleString();
+// let onlydate=new Date().toLocaleDateString();
+// let onlytime=new Date().toLocaleTimeString();
+
+// let timeOfUS=new Date().toLocaleString('en-US');
+
+// console.log(date);
